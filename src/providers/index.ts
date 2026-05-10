46a6990a51f9c0423d9d3b5b00c3ff82b8465057
Jan-David Salchow <jdsalchow@gmail.com>
2026-05-10 10:58:38 +0200

    fix: export plain-date-month-types

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,5 +1,7 @@
 export * from "./plain-date.js";
 export * from "./plain-date-time.js";
+export * from "./plain-month-day.js";
 export * from "./plain-time.js";
+export * from "./plain-year-month.js";
 export * from "./root.js";
 export * from "./zoned-date-time.js";
